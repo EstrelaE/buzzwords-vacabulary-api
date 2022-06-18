416015fdf4b6bd8b7b356eabcfa5afe40976f6e5
Estrela Espiridião <93261144+EstrelaE@users.noreply.github.com>
2022-06-18 12:24:19 +0100

    finish app features

--- js/main.js
+++ js/main.js
@@ -9,28 +9,46 @@ function getWords() {
     .then((data) => {
       console.log(data);
       document.querySelector(".words").innerHTML = "";
-      words = data.phrase.split(" ");
+      document.querySelector("#definition").innerText = "";
+      document.querySelector("#example").innerText = "";
+      words = data.phrase.replaceAll('"', "");
+      words = words.split(" ");
       words.forEach((word) => {
-        document.querySelector(
-          ".words"
-        ).innerHTML += `<li><a href="#">${word}</a></li>`;
+        const li = document.createElement("li");
+        li.textContent = word;
+        li.classList.add(word);
+        document.querySelector("ul").appendChild(li);
       });
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
-}
 
-document.querySelectorAll("a").addEventListener("click", getDefinition);
-function getDefinition() {
-  // const choice = document.querySelector("input").value;
-  const apiKey = "7323707fd8948f53cb2542f0d2e2657da03a78df";
-  const url = `https://owlbot.info/api/v4/dictionary/owl`;
+      //get definition
+      const lis = document.querySelectorAll("li");
+      lis.forEach((e) => e.addEventListener("click", getDefinition));
+      function getDefinition(e) {
+        let choice = e.srcElement.innerText;
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${choice}`;
+        fetch(url)
+          .then((res) => res.json()) // parse response as JSON
+          .then((data) => {
+            console.log(data[0]);
+            document.querySelector("#definition").innerText =
+              data[0].meanings[0].definitions[0].definition;
+            if (data[0].phonetic) {
+              document.querySelector("#phonetics").innerText = data[0].phonetic;
+            }
+            if (data[0].meanings[0].definitions[0].example) {
+              document.querySelector(
+                "#example"
+              ).innerText = `"${data[0].meanings[0].definitions[0].example}"`;
+            }
+          })
 
-  fetch(url)
-    .then((res) => res.json()) // parse response as JSON
-    .then((data) => {
-      console.log(data);
+          .catch((err) => {
+            console.log(`error ${err}`);
+            if (err) {
+              alert("can't help you with that one");
+            }
+          });
+      }
     })
     .catch((err) => {
       console.log(`error ${err}`);
